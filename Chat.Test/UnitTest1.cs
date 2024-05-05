b69375f249c105bc9b5bd15ec0bdec72be171576
muctepx <104897166+muctepx@users.noreply.github.com>
2024-05-05 16:48:17 +0300

    Update UnitTest1.cs

--- Chat.Test/UnitTest1.cs
+++ Chat.Test/UnitTest1.cs
@@ -27,13 +27,39 @@ namespace Chat.Test
             }
         }
 
+       [Test]
+        public async Task Test2()
+        {
+            var mock2 = new MockMessagesClient(55551);
+            var client = new ChatClient("Leonid", mock2);
+            mock2.InitializeClient(client);
+
+            client.Start();
+
+            using (var context = new ChatContext())
+            {
+                await Task.Delay(2000);
+                var first = context.Users.FirstOrDefault(x => x.Name == "Leonid");
+                Assert.IsNotNull(first);
+
+                Assert.IsTrue(first.FromMessages.Count == 1);
+                Assert.IsTrue(first.ToMessages.Count == 1);
+
+                var messageOne =
+                    context.Messages.FirstOrDefault(x => x.FromUser.Id == first.Id && x.ToUser.Id == first.Id);
+
+                Assert.AreEqual("Hello, Leonid", messageOne.Text);
+            }
+        }
+
+
         [Test]
         public async Task Test1()
         {
             var mock = new MockMessagesSource();
             var server = new ChatServer(mock);
             mock.InitializeServer(server);
-            await server.WorkAsync();
+            server.Work();
             using (var context = new ChatContext())
             {
                 Assert.IsTrue(context.Users.Count() == 2);
@@ -49,8 +75,10 @@ namespace Chat.Test
                 Assert.IsTrue(first.ToMessages.Count == 1);
                 Assert.IsTrue(second.ToMessages.Count == 1);
 
-                var messageOne = context.Messages.FirstOrDefault(x => x.FromUser.Id == first.Id && x.ToUser.Id == second.Id);
-                var messageTwo = context.Messages.FirstOrDefault(x => x.FromUser.Id == second.Id && x.ToUser.Id == first.Id);
+                var messageOne =
+                    context.Messages.FirstOrDefault(x => x.FromUser.Id == first.Id && x.ToUser.Id == second.Id);
+                var messageTwo =
+                    context.Messages.FirstOrDefault(x => x.FromUser.Id == second.Id && x.ToUser.Id == first.Id);
 
                 Assert.AreEqual("Hello, Ivan", messageTwo.Text);
                 Assert.AreEqual("Hello, Alex", messageOne.Text);
